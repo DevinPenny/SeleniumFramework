32cae341421144b1179e3a317b25365d9a0e7243
Devin <devin.penny@gmail.com>
2016-08-24 17:28:19 -0400

    minor cleanup

--- src/test/java/SeleniumTests/ExampleTestVerbose.java
+++ src/test/java/SeleniumTests/ExampleTestVerbose.java
@@ -1,78 +1,87 @@
 package SeleniumTests;
 
 import PageObjects.CommonObj;
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
+import PageObjects.MainPageObj;
+import org.junit.*;
+import org.junit.rules.TestWatcher;
+import org.junit.runner.Description;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+
 
 import java.io.IOException;
 import java.util.logging.*;
 
 public class ExampleTestVerbose {
 
-    WebDriver driver;
+WebDriver driver;
 
-    private static final Logger logger = Logger.getLogger(ExampleTestVerbose.class.getName());
-    public Handler fileHandler = null;
-    public Formatter formatter = null;
+private static final Logger logger = Logger.getLogger(ExampleTestVerbose.class.getName());
+public Handler fileHandler = null;
+public Formatter formatter = null;
 
-    public String FilePath = "./test.log";
+public String FilePath = "./test.log";
 
-    // Set up Logging and formatting for error handling
-    // instantiate the web driver
-    @Before
-    public void TestSetUp(){
+// Set up Logging and formatting for error handling
+// instantiate the web driver
+@Before
+public void TestSetUp() {
 
-       try {
-           fileHandler = new FileHandler(FilePath);
-       }catch (IOException e){
-           logger.info("Caught IOException: " + e);
-       }
-
-        formatter = new SimpleFormatter();
-
-        fileHandler.setLevel(Level.ALL);
-        fileHandler.setFormatter(formatter);
-
-        logger.addHandler(fileHandler);
-
-        driver = new FirefoxDriver();
-
-        logger.info("@Before test setup complete");
+    try {
+        fileHandler = new FileHandler(FilePath);
+    } catch (IOException e) {
+        logger.info("Caught IOException: " + e);
     }
 
+    formatter = new SimpleFormatter();
 
-    @After
-    public void testShutDown(){
-        logger.info("@After test shut down");
-        driver.close();
+    fileHandler.setLevel(Level.ALL);
+    fileHandler.setFormatter(formatter);
+
+    logger.addHandler(fileHandler);
+
+    driver = new FirefoxDriver();
+
+    logger.info("@Before test setup complete");
+}
+
+
+@After
+public void testShutDown() {
+    logger.info("@After test shut down");
+    driver.close();
+}
+
+@Rule
+public TestWatcher listen = new TestWatcher() {
+
+    @Override
+    public void failed(Throwable t, Description description) {
+        System.out.println("Test Failed!");
+        System.out.println(description.getClassName() + ", " + description.getMethodName());
     }
 
+};
     // Navigate to the web page, perform an action and verify the result
     @Test
     public void FirstTestToExecute() {
         logger.info("FirstTestToExecute");
 
-        CommonObj AbstractObject = new CommonObj(driver);
+        CommonObj Navigation = new CommonObj(driver);
+        MainPageObj MainPage = new MainPageObj(driver);
+
+        //Navigate to page
+        Navigation.NavigateToPage();
 
 
-        //MainPage MainPageObject = new MainPage(driver);
-        //LoginPageObject LoginPageObject = new LoginPageObject(driver);
+        //Do something on the page
+        MainPage.EnterTextInField();
 
+        MainPage.ClickButton();
 
-        //navigate to map view
-        //MapViewAbstractObject.NavigateToMapView();
-
-        //enter user credentials (username/password)
-        //MapViewAbstractObject.EnterLoginCredentials1();
-
-        //click the log in button
-        //MapViewAbstractObject.ClickLogIn1();
 
         //create some sort of assert
-        //Assert.assertTrue(MainPageObject.GetTextFromPage().contains("Some Message"));
+        Assert.assertTrue(MainPage.GetTextFromPage().contains("Some Message"));
 
     }
 
